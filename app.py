9fa674f3526c1ab26889339a26f99567d78242aa
vasiliy <vash050@rmbler.ru>
2023-02-15 17:03:37 +0300

    -json to v1

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():  # put application's code here
+    return render_template("index.html")
+
+
+@app.route('/v1/', methods=['POST'])
+def get_cell():
+    print(request.data.decode())
     return render_template("index.html")
 
 
--- static/js/go_to_from_server.js
+++ static/js/go_to_from_server.js
@@ -4,6 +4,23 @@ function sendToServer() {
     let button_send = document.querySelector('.send');
     button_send.addEventListener('click', function () {
         console.log(data.arrayMan);
-    })
+        let result = '';
+        let xhr = new XMLHttpRequest();
+        let url = 'http://127.0.0.1:5000/v1';
 
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+         xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+
+                    // Print received data from server
+                    result = this.responseText;
+                    console.log(result)
+                }
+            };
+
+         let data_j = JSON.stringify({ "man_coord": data.arrayMan,});
+         xhr.send(data_j);
+    })
 }
